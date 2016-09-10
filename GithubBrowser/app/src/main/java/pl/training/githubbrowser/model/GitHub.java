3771594442e7c7c330936ae5689e419ebef4beb9
fist <fist@ericpol.com>
2016-09-10 11:31:11 +0200

    Imported xmls from Lukasz.

--- GithubBrowser/app/src/main/java/pl/training/githubbrowser/model/GitHub.java
+++ GithubBrowser/app/src/main/java/pl/training/githubbrowser/model/GitHub.java
@@ -1,11 +1,37 @@
 package pl.training.githubbrowser.model;
 
-import android.database.Observable;
+
+import java.util.List;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Url;
+import rx.Observable;
 
 /**
  * Created by fist on 2016-09-10.
  */
 public interface GitHub {
 
-	Observable<>
+	@GET("users/{username}/repos")
+	Observable<List<Repository>> publicRepositories(@Path("username") String user);
+
+	@GET
+	Observable<User> userFromUrl(@Url String userUrl);
+
+	class Factory {
+
+		public static GitHub create() {
+			Retrofit retrofit = new Retrofit.Builder()
+					.baseUrl("https://api.github.com")
+					.addConverterFactory(GsonConverterFactory.create())
+					.addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+					.build();
+			return retrofit.create(GitHub.class);
+		}
+	}
 }
+
